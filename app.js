d064723878aa27cfc454459ff2f4e1d9c75f2791
Campillay Sebastian <israelcampy@gmail.com>
2022-09-09 18:29:28 -0300

    Ejercicio 1 resuelto y probado

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const countries = require('./countries.json');
  * @returns {Object}
  */
 function getCountryByIso3(isoCode){
-    //Complete with your code
+    for(var c in countries) {
+        if(countries[c].iso3==isoCode){
+            return countries[c].name;
+        }
+     }
 }
 
 /**
